ccc72bd6749e053926196270ad29fb8a239f18c8
RomeoBhuiyan <55655775+Discord-Chan@users.noreply.github.com>
2022-10-20 22:47:51 +0200

    Update about.js

--- Animotion_nextjs/pages/about.js
+++ Animotion_nextjs/pages/about.js
@@ -3,7 +3,7 @@ export default function About() {
   	<>
     <p id="main-headline">About us</p>
     <p id="main-headline">We're really great all around</p>
-    <p id="main-headline">To be fair I'm doing all the work</p>
+    <p id="main-headline">Odd Balls</p>
     <p id="main-headline">But we in this together</p>
     </>
   )
