14e5b9e969575972781c8994d7ebfc80072fdbda
ChristophLasinger <lasingerch@gmail.com>
2022-10-20 23:18:21 +0200

    update mediaplayer

--- Animotion_nextjs/pages/player.js
+++ Animotion_nextjs/pages/player.js
@@ -4,9 +4,17 @@ export default function Player() {
 		<p id="main-headline">Mediaplayer</p>
 		
 		<div id="video-container">
-			<div className="dance">
+			<div className="dance-container">
 				<img className="dance-image" src="/images/dances/dance1.jpg"></img>
 				<p className="dance-name">dance1</p>
+			</div>
+			<div className="dance-container">
+				<img className="dance-image" src="/images/dances/dance1.jpg"></img>
+				<p className="dance-name">dance2</p>
+			</div>
+			<div className="dance-container">
+				<img className="dance-image" src="/images/dances/dance1.jpg"></img>
+				<p className="dance-name">dance3</p>
 			</div>
 		</div>
 		</>
