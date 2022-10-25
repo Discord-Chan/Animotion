1f2c18d557fca6887a04528926e4511df823670f
RomeoBhuiyan <55655775+Discord-Chan@users.noreply.github.com>
2022-10-25 18:18:21 +0200

    added toogle windows button
    - changed links to buttons
    - added some onclick functions
    - better performance

---
+++ Animotion_nextjs/public/webapp/scripts/options.js
@@ -0,0 +1,24 @@
+function get_vision() {
+
+    //var t = document.getElementById("give_vision");
+
+    if (document.getElementById("return_vision").style.display == "none") {
+      document.getElementById("return_vision").style.display = "block";
+    } else {
+      document.getElementById("return_vision").style.display = "none";
+    }
+
+    if (document.getElementById("record_vision").style.display == "none") {
+        document.getElementById("record_vision").style.display = "block";
+      } else {
+        document.getElementById("record_vision").style.display = "none";
+      }
+
+
+    if (document.getElementById("preview_vision").style.display == "none") {
+        document.getElementById("preview_vision").style.display = "block";
+    } else {
+        document.getElementById("preview_vision").style.display = "none";
+    }
+
+  }
