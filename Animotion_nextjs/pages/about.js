1f20ec5687d281c0afc01178a100c8fdd848cdc3
ChristophLasinger <lasingerch@gmail.com>
2022-10-25 17:06:37 +0200

    change vrm images and home page element distances

--- Animotion_nextjs/pages/about.js
+++ Animotion_nextjs/pages/about.js
@@ -2,9 +2,9 @@ export default function About() {
 	return (
   	<>
     <p id="main-headline">About us</p>
-    <p id="main-headline">We're really great all around</p>
-    <p id="main-headline">Odd Balls</p>
-    <p id="main-headline">But we in this together</p>
+    <p>We're really great all around</p>
+    <p>Odd Balls</p>
+    <p>But we in this together</p>
     </>
   )
 }
