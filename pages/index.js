d5cd3c3a22ca2861698b06470acd3968e2ea6211
ChristophLasinger <lasingerch@gmail.com>
2022-12-30 18:57:58 +0100

    remove other projects and reorganize repo

---
+++ pages/index.js
@@ -0,0 +1,99 @@
+function SelectModel(model) {
+  window.localStorage.setItem("vrm", model);
+  GoTo("webapp/index.html");
+}
+
+function GoTo(link) {
+  window.location.href = link;
+}
+
+export default function Home() {
+  return (
+    <>
+      <p id="logo" className="glitch" data-text="Animotion">
+        Animotion
+      </p>
+      <div className="vrm-container">
+        <button
+          className="vrm-selector glow-on-hover"
+          type="button"
+          onClick={() => {
+            SelectModel(
+              "https://cdn.glitch.global/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/cyberlady.vrm?v=1671406275496"
+            );
+          }}
+        >
+          <img
+            className="vrm-image"
+            src="/images/vrms/yor2_blackbackground.jpg"
+            alt="virtual model black haired boy with glasses"
+          />
+        </button>
+
+        <button
+          className="vrm-selector glow-on-hover"
+          type="button"
+          onClick={() => {
+            SelectModel(
+              "https://cdn.glitch.global/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/felix.vrm?v=1666724550269"
+            );
+          }}
+        >
+          <img
+            className="vrm-image"
+            src="https://cdn.glitch.global/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/vampire_blackbackground.jpg?v=1672419201319"
+            alt="virtual model black haired boy with glasses"
+          />
+        </button>
+
+        <button
+          className="vrm-selector glow-on-hover"
+          type="button"
+          onClick={() => {
+            SelectModel(
+              "https://cdn.glitch.global/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/girl_rainbow.vrm?v=1666725244245"
+            );
+          }}
+        >
+          <img
+            className="vrm-image"
+            src="https://cdn.glitch.global/14365248-98f0-4e5a-a5a3-1a7cd5ad47c8/lgtbq_girl_blackbackground.jpg?v=1672419182513"
+            alt="virtual model black haired boy with glasses"
+          />
+        </button>
+      </div>
+
+      <div className="link-container">
+        <button
+          className="link-button"
+          role="button"
+          id="player-button"
+          onClick={() => {
+            GoTo("player");
+          }}
+        >
+          Mediaplayer
+        </button>
+        <button
+          className="link-button"
+          role="button"
+          id="about-button"
+          onClick={() => {
+            GoTo("about");
+          }}
+        >
+          Among us
+        </button>
+        <button
+          className="link-button"
+          role="button"
+          onClick={() => {
+            GoTo("community");
+          }}
+        >
+          Community
+        </button>
+      </div>
+    </>
+  );
+}
