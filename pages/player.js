581203f4d11389e65cb0e945548c873c78e09017
ChristophLasinger <lasingerch@gmail.com>
2023-01-15 18:28:20 +0100

    fix property name warnings

--- pages/player.js
+++ pages/player.js
@@ -5,16 +5,16 @@ export default function Player() {
 		
 		<div className="video-container">
 			<div className="dance-container">
-				<iframe className="youtube-video-iframe" src="https://www.youtube.com/embed/O36YAImtk4E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<iframe className="youtube-video-iframe" src="https://www.youtube.com/embed/O36YAImtk4E" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<p className="dance-name">The Feels - Animotion</p>
 			</div>
 			<div className="dance-container">
-			<iframe className="youtube-video-iframe" src="https://www.youtube.com/embed/muWjGi8tvX4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe className="youtube-video-iframe" src="https://www.youtube.com/embed/muWjGi8tvX4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
 				<p className="dance-name">Feel Special - Animotion</p>
 			</div>
 			<div className="dance-container">
-			<iframe className="youtube-video-iframe" src="https://www.youtube.com/embed/7W-4_qw41Ek" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe className="youtube-video-iframe" src="https://www.youtube.com/embed/7W-4_qw41Ek" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				
 				<p className="dance-name">Devil - Animotion</p>
 			</div>
@@ -22,17 +22,17 @@ export default function Player() {
 		<div className="video-container">
 			{/*
 		 <div className="dance-container">
-			<iframe className="youtube-video-iframe" src="https://www.youtube-nocookie.com/embed/U5cK0gVOz_Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe className="youtube-video-iframe" src="https://www.youtube-nocookie.com/embed/U5cK0gVOz_Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				
 				<p className="dance-name">dance 4</p>
 			</div>
 			<div className="dance-container">
-			<iframe className="youtube-video-iframe" src="https://www.youtube-nocookie.com/embed/U5cK0gVOz_Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe className="youtube-video-iframe" src="https://www.youtube-nocookie.com/embed/U5cK0gVOz_Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				
 				<p className="dance-name">dance 5</p>
 			</div>
 			<div className="dance-container">
-			<iframe className="youtube-video-iframe" src="https://www.youtube-nocookie.com/embed/U5cK0gVOz_Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe className="youtube-video-iframe" src="https://www.youtube-nocookie.com/embed/U5cK0gVOz_Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				
 				<p className="dance-name">dance 6</p>
 			</div>
